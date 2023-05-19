4192b31e6e644ad936d8e12c09e849130a47e9ec
Dev-Franqqi <esendufranklinebi@gmail.com>
2023-05-19 15:26:15 +0100

    added whatsapp component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,8 @@ import Thirdcomponent from "../components/Homepage/Thirdcomponent";
 import Secondsection from "../components/Homepage/Secondsection";
 import Fourthsection from "../components/Homepage/Fourthsection";
 import Footer from "../components/Homepage/Footer";
-
+import { Link } from "react-router-dom";
+import whatsapps from "../assets/whatsapp.png"
 export default function Home() {
   const transits:Transition ={
     opacity:1,
@@ -18,13 +19,27 @@ export default function Home() {
       hidden:{opacity:0},
       visible:{opacity:1}
     }
-4
+    const whatsappt:Transition = {
+      opacity:1,
+      delay:4,
+      duration:6,
+      ease:'easeIn'
+
+    }
+
 
   return (
     <motion.div initial={'hidden'} animate={'visible'} variants={vari} transition={transits} className="font-sans text-white">
-
+     
    <CryptoList />
    <Navbar />
+
+   <motion.div initial='hidden' animate='visible' variants={vari} transition={whatsappt} className="bg-gray-200 md:bottom-2 fixed bottom-72 right-0  md:right-0 w-40 rounded "><Link className="flex" to='https://wa.me/+2347047669823'>
+     <img src={whatsapps} width={50} alt="" />
+     <span className=" text-black self-center">Chat with us</span>
+   </Link>
+
+      </motion.div> 
     
         {/* <img className="mx-auto" src={building} alt="" /> */}
       <Initial />
