e45c635a0959dbda335052e90305368a31bc270f
Franklin Ebi <esendufranklinebi@gmail.com>
2025-02-28 11:42:15 +0000

    hedden

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,63 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom"
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
-import Home from "./pages/Home"
-import Dashboard from "./pages/Dashboard"
-import Deposit from "./pages/Deposit"
-import "./App.css"
-import About from "./pages/About"
-import Education from "./pages/Education/Education"
-import Intermediate from "./pages/Education/Advanced"
-import Advanced from "./pages/Education/Advanced"
-import Withdraw from "./pages/Withdraw"
-import Pricing from "./pages/Pricing"
+// import { BrowserRouter,Routes,Route } from "react-router-dom"
+// import Login from "./pages/Login"
+// import Signup from "./pages/Signup"
+// import Home from "./pages/Home"
+// import Dashboard from "./pages/Dashboard"
+// import Deposit from "./pages/Deposit"
+// import "./App.css"
+// import About from "./pages/About"
+// import Education from "./pages/Education/Education"
+// import Intermediate from "./pages/Education/Advanced"
+// import Advanced from "./pages/Education/Advanced"
+// import Withdraw from "./pages/Withdraw"
+// import Pricing from "./pages/Pricing"
 
-import Settings from "./pages/Settings"
-export default function App() {
-  return (
-   <BrowserRouter>
-   <Routes>
-    <Route path='/' element= {<Home />} />
-    <Route path='/login' element= {<Login />} />
-    <Route path='/signup' element= {<Signup />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/dashboard/withdraw' element={<Withdraw />} />
-        <Route path='/dashboard/settings' element={<Settings />} />
-        <Route path="/deposit" element={<Deposit />} />
-        <Route path="/education" element={<Education />} />
-        <Route path="/education/intermediate" element={<Intermediate />} />
-        <Route path="/education/advanced" element={<Advanced />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/pricing" element={<Pricing />} />
+// import Settings from "./pages/Settings"
+// export default function App() {
+//   return (
+//    <BrowserRouter>
+//    <Routes>
+//     <Route path='/' element= {<Home />} />
+//     <Route path='/login' element= {<Login />} />
+//     <Route path='/signup' element= {<Signup />} />
+//         <Route path='/dashboard' element={<Dashboard />} />
+//         <Route path='/dashboard/withdraw' element={<Withdraw />} />
+//         <Route path='/dashboard/settings' element={<Settings />} />
+//         <Route path="/deposit" element={<Deposit />} />
+//         <Route path="/education" element={<Education />} />
+//         <Route path="/education/intermediate" element={<Intermediate />} />
+//         <Route path="/education/advanced" element={<Advanced />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/pricing" element={<Pricing />} />
 
 
-   </Routes>
-   </BrowserRouter>
-  )
-}
-
-// export default function App(){
-//   return(
-//     <>
-    
-    
-//     <h1 className="text-center font-bold mt-36 text-2xl">Website not available at the moment..</h1>
-//     <p className="text-center mt-2 text-sm">Please check your web host </p>
-//     </>
+//    </Routes>
+//    </BrowserRouter>
 //   )
 // }
+
+// // export default function App(){
+// //   return(
+// //     <>
+    
+    
+// //     <h1 className="text-center font-bold mt-36 text-2xl">Website not available at the moment..</h1>
+// //     <p className="text-center mt-2 text-sm">Please check your web host </p>
+// //     </>
+// //   )
+// // }
+
+export default function Home(){
+
+
+  return(
+
+    <main className="text-center mt-20">
+
+    <h1 className="text-2xl mb-2 font-bold">Web Page is currently down </h1>
+    <p>Contact service provider!</p>
+
+
+    </main>
+  )
+}
